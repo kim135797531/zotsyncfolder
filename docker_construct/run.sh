045d6218588bb4414a904aa2f761af6523bcc77a
Dongmin Kim <kim135797531g@gmail.com>
2021-05-08 19:45:27 +0900

    Set docker timezone to seoul

--- docker_construct/run.sh
+++ docker_construct/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 set -e
+date
+TZ=Asia/Seoul
+date
 
 cd $ZOTSYNCFOLDER_HOME
-/opt/conda/bin/conda run -n $CONDA_ENV_NAME python construct_folder.py
+/opt/conda/bin/conda run -n $CONDA_ENV_NAME sh -c 'python construct_folder.py > /dev/tty 2>&1'
--- docker_watcher/run.sh
+++ docker_watcher/run.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 set -e
+date
+TZ=Asia/Seoul
+date
 
 cd $ZOTSYNCFOLDER_HOME
-/opt/conda/bin/conda run -n $CONDA_ENV_NAME python file_watcher.py
+/opt/conda/bin/conda run -n $CONDA_ENV_NAME sh -c 'python file_watcher.py > /dev/tty 2>&1'
